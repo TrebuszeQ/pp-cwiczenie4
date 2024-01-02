58817b1aa4b643280924f199e3fd93a44012d33d
TrebuszeQ <trebuszeq@gmail.com>
2024-01-02 18:20:44 +0100

    02.01.24

--- main.cpp
+++ main.cpp
@@ -1,7 +1,8 @@
 #include <iostream>
 #include <string>
-#include <list>
-#include <math.h>
+#include <array>
+#include <cmath>
+#include <random>
 
 using namespace std;
 
@@ -123,92 +124,92 @@ float read_flt_positive(string message) {
     return stof(n);
 }
 
-// wyswietla zawartosc tablicy
-void print_list(list<int> lis) {
-    if (lis.size() > 0) {
-        cout << "Lista: ";
-        int i = 0;
-
-        for (int elem : lis) {
-            cout << elem;
-
-            if (i == (lis.size() - 1)) cout << ".\n";
-            else cout << ", ";
-            i++;
-        }
-    }
-
-    else cout << "Lista jest pusta.\n";
-}
-
-
-// funkcja wczytuje n liczb calkowitych do listy
-list<int> read_n_ints(int n) {
-    list<int> lis;
-
-    if (n > 0) {
-        for (int i = 0; i < n; i++) {
-            lis.push_front(read_int_negative("Wprowadz element listy.\n"));
-        }
-
-    }
-
-    else cout << "Lista jest pusta.\n";
-
-    // cout << "check" << endl;
-    // for (int i = 0; i < n; i++) {
-    //    cout << result[i] << ";";
-    // }
-
-    return lis;
-}
-
-
-// funkcja dodaje do siebie dodatnie zawartosci args. Zwraca sume i ilosc liczb dodatnich.
-int * sum_positive_int(list<int> lis) {
-    int sum = 0;
-    int count = 0;
-
-
-    for (int elem : lis) {
-        if (elem > 0) {
-            sum += elem;
-            count++;
-        }
-    }
-
-    static int result[2] = { sum, count };
-
-    return result;
-}
-
-
-// funkcja dodaje do siebie ujemne zawartosci args. Zwraca srednia arytmetyczna i ilosc liczb ujemnych.
-float * sum_negative_int(list<int> lis) {
-    int avg = 0;
-    int count = 0;
-
-    for (int elem : lis) {
-        if (elem < 0) {
-            avg += elem;
-            count++;
-        }
-    }
-
-    static float result[2];
-
-    if (count > 0) {
-        result[0] = abs((1.0f * avg) / (1.0f * count));
-        result[1] = count;
-    }
-
-    else {
-        result[0] = 0;
-        result[1] = count;
-    }
-
-    return result;
-}
+//// wyswietla zawartosc tablicy
+//void print_list(list<int> lis) {
+//    if (lis.size() > 0) {
+//        cout << "Lista: ";
+//        int i = 0;
+//
+//        for (int elem : lis) {
+//            cout << elem;
+//
+//            if (i == (lis.size() - 1)) cout << ".\n";
+//            else cout << ", ";
+//            i++;
+//        }
+//    }
+//
+//    else cout << "Lista jest pusta.\n";
+//}
+//
+//
+//// funkcja wczytuje n liczb calkowitych do listy
+//list<int> read_n_ints(int n) {
+//    list<int> lis;
+//
+//    if (n > 0) {
+//        for (int i = 0; i < n; i++) {
+//            lis.push_front(read_int_negative("Wprowadz element listy.\n"));
+//        }
+//
+//    }
+//
+//    else cout << "Lista jest pusta.\n";
+//
+//    // cout << "check" << endl;
+//    // for (int i = 0; i < n; i++) {
+//    //    cout << result[i] << ";";
+//    // }
+//
+//    return lis;
+//}
+//
+//
+//// funkcja dodaje do siebie dodatnie zawartosci args. Zwraca sume i ilosc liczb dodatnich.
+//int * sum_positive_int(list<int> lis) {
+//    int sum = 0;
+//    int count = 0;
+//
+//
+//    for (int elem : lis) {
+//        if (elem > 0) {
+//            sum += elem;
+//            count++;
+//        }
+//    }
+//
+//    static int result[2] = { sum, count };
+//
+//    return result;
+//}
+//
+//
+//// funkcja dodaje do siebie ujemne zawartosci args. Zwraca srednia arytmetyczna i ilosc liczb ujemnych.
+//float * sum_negative_int(list<int> lis) {
+//    int avg = 0;
+//    int count = 0;
+//
+//    for (int elem : lis) {
+//        if (elem < 0) {
+//            avg += elem;
+//            count++;
+//        }
+//    }
+//
+//    static float result[2];
+//
+//    if (count > 0) {
+//        result[0] = abs((1.0f * avg) / (1.0f * count));
+//        result[1] = count;
+//    }
+//
+//    else {
+//        result[0] = 0;
+//        result[1] = count;
+//    }
+//
+//    return result;
+//}
 
 
 // oblicza i zwracaja pole powierzchni trapezu:
@@ -229,14 +230,37 @@ void zadanie2(float x) {
     printf("Wartosc funkcji f(x) w punkcie %0.2f wynosi %0.2f.\n\n", x, y);
 }
 
+array<int, 2> coinToss(int x) {
+    cout << "Program symuluje rzut monetą n podanych razy.";
+
+    int o = 0;
+    int r = 0;
+    // https://learn.microsoft.com/pl-pl/cpp/standard-library/random?view=msvc-170
+    random_device rd;
+    mt19937 gen(rd());
+    uniform_int_distribution<> dist(1, 2);
+    for (int i = 0; i < x; i++) {
+        if (dist(gen) != 1) {
+            r++;
+            cout << "Reszka\n";
+        }
+
+        else {
+            o++;
+            cout << "Orzel\n";
+        }
+    }
+
+    return {r, o};
+}
 
 int main() {
     while (true) {
         cout << "Opcje:\n";
-        cout << "1. Zadanie 1.3.1. - pole trapezu\n";
-        cout << "2. Zadanie 1.3.2. - wartosc funkcji w punkcie\n";
-        cout << "3. Zadanie 1.3.3. - rzut moneta\n";
-        cout << "4. Zadanie 1.3.4. - pole trojkata z odcinkow\n";
+        cout << "1. Zadanie 1.4.1. - pole trapezu\n";
+        cout << "2. Zadanie 1.4.2. - wartosc funkcji w punkcie\n";
+        cout << "3. Zadanie 1.4.3. - rzut moneta\n";
+        cout << "4. Zadanie 1.4.4. - pole trojkata z odcinkow\n";
         cout << "5. Wyjscie.\n\n";
 
         int opt = read_int_positive("Wprowadz numer opcji [od 0 - 5].\n");
@@ -250,9 +274,13 @@ int main() {
                 zadanie2(read_flt("Wprowadz liczbe rzeczywista.\n"));
                 break;
 
-            case 3:
+            case 3: {
+                array<int, 2> res = {0, 0};
+                res = coinToss(read_int_positive("Wprowadz dodatnia liczbe calkowita.\n"));
+                printf("Reszka wypadla %d razy, orzel wypadl %d razy.", res[0], res[1]);
                 break;
-
+            }
+            
             case 4:
                 break;
 
