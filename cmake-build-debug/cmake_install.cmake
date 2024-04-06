cc3b08afdae411c82ff23bfd5c6067cb13c82680
TrebuszeQ <trebuszeq@gmail.com>
2024-04-06 17:18:57 +0200

    final

--- cmake-build-debug/cmake_install.cmake
+++ cmake-build-debug/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: /home/trebuszeq/Cpp/cwiczenie4
+# Install script for directory: /home/share/Cpp/cwiczenie4
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -50,5 +50,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "/home/trebuszeq/Cpp/cwiczenie4/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "/home/share/Cpp/cwiczenie4/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
